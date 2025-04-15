38f93edfa6a2616773654e34f95c805eb1950620
Gubio Garcia <gubiogarcia@gmail.com>
2025-04-15 20:23:24 -0300

    correção do uso do swagger

--- Contatos/src/main/java/eng/api/Contatos/ContatosApplication.java
+++ Contatos/src/main/java/eng/api/Contatos/ContatosApplication.java
@@ -2,7 +2,10 @@ package eng.api.Contatos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 
+@OpenAPIDefinition(info = @io.swagger.v3.oas.annotations.info.Info(title = "${info.build.name}", version = "${info.build.version}", description = "${info.app.description}",
+		contact = @io.swagger.v3.oas.annotations.info.Contact(name = "", email = "")))
 @SpringBootApplication
 public class ContatosApplication {
 
